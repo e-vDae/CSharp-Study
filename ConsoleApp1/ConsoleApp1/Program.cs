12d5569df510c5a86afc6e5add55bdd19029d943
e-vDae <e.vitto@dewan-architects.com>
2025-02-09 12:19:32 +0800

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class SalaryIncrease
+{
+    static void Main()
+    {
+        Console.Title = "SalaryIncreasonator";
+        decimal Salary;
+        decimal NewSalary;
+        string Currency = "Riyals";
+
+        Salary = 10M;
+        NewSalary = SalaryIncreasonator1000();
+
+        if (NewSalary < 9_999_999)
+        {
+            Console.WriteLine("\nThat's too low habibi, you still boor berson :(");
+        }
+        else
+        {
+            Console.WriteLine($"\nYour previous salary is {Salary} {Currency} :(");
+            Console.WriteLine($"Your salary is now {NewSalary} {Currency}, Shukran Habibi, You rich now, Buy Lamborghini!");
+        }
+
+        Console.ReadKey();
+    }
+    static decimal SalaryIncreasonator1000()
+    {
+        Console.Write("Input your asking salary habibi : ");
+        decimal HowMuchHabibi = Convert.ToDecimal(Console.ReadLine());
+        return HowMuchHabibi;
+    }
+
+    static decimal SalaryIncreasonator3000()
+	{
+        Console.Write("Input your asking salary habibi ");
+        decimal HowMuchHabibi = Convert.ToDecimal(Console.ReadLine());
+
+        while (HowMuchHabibi < 9_999_999_999)
+            {
+                Console.Write("\nThat's too low habibi, add more blease... ");
+                HowMuchHabibi = Convert.ToDecimal(Console.ReadLine());
+            }
+
+        return HowMuchHabibi;
+    }
+
+}
